d0eeb073a8d633f87e0b16427385e24fa6e63660
BrookeSherman03 <112006636+BrookeSherman03@users.noreply.github.com>
2024-02-08 13:02:19 -0500

    Update requirements.R

--- required/requirements.R
+++ required/requirements.R
@@ -1,4 +1,2 @@
-install.packages("data.table", "http://lib.stat.cmu.edu/R/CRAN/")
-install.packages("dpylr",  "http://lib.stat.cmu.edu/R/CRAN/")
-install.packages("Metrics",  "http://lib.stat.cmu.edu/R/CRAN/")
-install.packages("readr",  "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("data.table",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("optparse",repos= "http://lib.stat.cmu.edu/R/CRAN/")
