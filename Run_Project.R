6002157aea3d31d69f53760c1792be68b6c4eac2
BrookeSherman03 <112006636+BrookeSherman03@users.noreply.github.com>
2024-02-15 15:28:05 -0500

    Update Run_Project.R

--- Run_Project.R
+++ Run_Project.R
@@ -1,2 +1,2 @@
-source("C:/Users/Groot/OneDrive - The Pennsylvania State University/Documents/Coin_Project/src/features/feature_load.R")
-source("C:/Users/Groot/OneDrive - The Pennsylvania State University/Documents/Coin_Project/src/models/model.R")
+source("./Coin_Project/src/features/feature_load.R")
+source("./Coin_Project/src/models/model.R")
