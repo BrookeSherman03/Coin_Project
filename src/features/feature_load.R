737f5d4d82923bb65747e069f806cd9ab5d2b708
BrookeSherman03 <112006636+BrookeSherman03@users.noreply.github.com>
2024-02-15 12:55:26 -0500

    Update feature_load.R

--- src/features/feature_load.R
+++ src/features/feature_load.R
@@ -4,30 +4,22 @@ library(data.table)
 library(Metrics)
 
 #load data
-#train <- fread("./Coin_Project/volume/data/raw/train_file.csv")
-#test <- fread("./Coin_Project/volume/data/raw/test_file.csv")
-train <- fread("C:/Users/Groot/OneDrive - The Pennsylvania State University/Documents/Coin_Project/volume/data/raw/train_file.csv")
-test <- fread("C:/Users/Groot/OneDrive - The Pennsylvania State University/Documents/Coin_Project/volume/data/raw/test_file.csv")
+train <- fread("./Coin_Project/volume/data/raw/train_file.csv")
+test <- fread("./Coin_Project/volume/data/raw/test_file.csv")
 
-id <- test$id
+#will be needed for the model
 train_result <- train$result
 
+#drop columns not needed for rowSum
 newtrain <- subset(train, select = -c(id, result))
 newtest <- subset(test, select = -c(id))
 
+#utilize rowSum
 newtrain <- data.table(rowSums(newtrain))
 newtest <- data.table(rowSums(newtest))
 
+#keep the result to test the model
 newtrain$result <- train_result
 
-glm_model<-glm(result~.,family=binomial,data=newtrain)
-saveRDS(glm_model, "C:/Users/Groot/OneDrive - The Pennsylvania State University/Documents/Coin_Project/volume/models/glm_model")
-
-summary(glm_model)
-coef(glm_model)
-
-test$pred<-predict(glm_model,newdata = newtest,type="response")
-
-submit <- data.table(id)
-submit$result <- test$pred
-fwrite(submit, "C:/Users/Groot/OneDrive - The Pennsylvania State University/Documents/Coin_Project/volume/data/processed/submit.csv")
+newtrain <- fwrite("./Coin_Project/volume/data/interim/train_file.csv")
+newtest <- fwrite("./Coin_Project/volume/data/interim/train_file.csv")
