6573fe43933e8f75768a6e45ef1ec40fa28eccfd
Brooke Sherman <bes5659@psu.edu>
2024-02-15 13:05:37 -0500

    change

--- Run_Project.R
+++ Run_Project.R
@@ -1,2 +1,2 @@
-source("C:/Users/Groot/OneDrive - The Pennsylvania State University/Coin_Project/src/features/feature_load")
-source("C:/Users/Groot/OneDrive - The Pennsylvania State University/Coin_Project/src/models/model")
+source("C:/Users/Groot/OneDrive - The Pennsylvania State University/Coin_Project/src/features/feature_load.R")
+source("C:/Users/Groot/OneDrive - The Pennsylvania State University/Coin_Project/src/models/model.R")
